dd369babe17f02f6833572e23fda3fd8296efcfb
Qi Tian <44391862+TQBio@users.noreply.github.com>
2021-11-16 16:26:13 +0800

    Update Visualization.r

--- scMelody/Visualization.r
+++ scMelody/Visualization.r
@@ -1,24 +1,39 @@
 
-###Using UMAP and hierarchical clustering for visualization in R###
+###Using hierarchical clustering heatmap to visualize the weighted consensus matrix in R###
 
-library(umap)
+###Taking the dataset of GSE87197 for example####
+
 library(pheatmap)
 
+cell_bd  <- c(rep('CLP',times=21),rep('CMP',times=19),rep('GMP',times=22),
+              rep('HSC',times=18),rep('MLP0',times=24),rep('MPP',times=18))
 
-UMAP_plot <- function(CA,cell_label){
-     em_bd <- umap(1-CA,input='dist',n_components=2)$layout
-     df_plot <- data.frame(cell_label,em_bd[,1],em_bd[,2])
-     colnames(df_plot) <- c("Data","Dimension1","Dimension2")
-     df_plot$Data<- factor(df_plot$Data)
-     plot_umap <- ggplot(df_plot, aes(x = Dimension1, y = Dimension2,color=Data,shape=Data)) +
-                                        geom_point(size=3)+
-            		        scale_color_brewer(palette = "Set2")+
-            		        scale_shape_manual(values = c(0,1,2,5,6,7))
-            		        theme_set(theme_bw())
-    return(plot_umap)
-   }
+inx_1 <- c(paste('GSM23244',43:63,sep =''))
+inx_1 <- c(paste(inx_1,"CLP",1:21,sep ='_'))
+inx_2 <- c(paste('GSM2324',497:515,sep =''))
+inx_2 <- c(paste(inx_2,"CMP",1:19,sep ='_'))
+inx_3 <- c(paste('GSM2324',549:570,sep =''))
+inx_3 <- c(paste(inx_3,"GMP",1:22,sep ='_'))
+inx_4 <- c(paste('GSM2324',625:642,sep =''))
+inx_4 <- c(paste(inx_4,"HSC",1:18,sep ='_'))
+inx_5 <- c(paste('GSM2324',815:838,sep =''))
+inx_5 <- c(paste(inx_5,"MLP0",1:24,sep ='_'))
+inx_6 <- c(paste('GSM2324',977:994,sep =''))
+inx_6 <- c(paste(inx_6,"MPP",1:18,sep ='_'))
+inx_bd<- c(inx_1,inx_2,inx_3,inx_4,inx_5,inx_6)
 
 hc_heatmap <- function(CA){
    return(pheatmap(as.matrix(CA),cutree_rows = NA,cutree_cols=NA,
                     cellwidth = 8, cellheight = 8))
 }
+
+bd_df <- data.frame(cons_Mat)
+rownames(bd_df) <- inx_bd
+colnames(bd_df) <- inx_bd
+annotation <- data.frame(cell_bd,pre_cluster) 
+colnames(annotation) <- c("true clusters","inferred clusters")
+rownames(annotation) <- inx_bd
+tiff(filename = "Heatmap_of_gse87197.tiff", res = 600, width = 4500, height = 4000,compression = 'lzw')
+pheatmap(as.matrix(bd_df),cutree_rows = NA,cutree_cols=NA,
+         annotation_col = annotation,cellwidth = 3, cellheight = 3, fontsize =4)
+dev.off()
